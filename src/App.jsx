21b552859367c4feaf3ff8c64204b4d99d2689d9
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-03-03 09:30:04 +0100

    small progress

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
 
   return (
     <>
-      <h1 style={{ fontSize: "5rem" }}>Current score : {score}</h1>
+      <h1 style={{ fontSize: "5rem" , color: "teal"}}>Current score : {score}</h1>
       {mainMenu ?
         <PlayerChoice clickedUserChoice={playerChoose} />
 
@@ -62,9 +62,9 @@ function App() {
 
         :
         <div>
-          <h1 style={{ fontSize: "5rem" }}>You choose <span style={{ color: "red" }}>{playerChoice}</span> and the computer chose <span style={{ color: "gold" }}>{botChoice}</span></h1>
-          <button style={{ fontSize: "5rem", cursor: "pointer" }} onClick={reset}>reset</button>
-          <h1 style={{ fontSize: "5rem", textDecoration: "underlined" }}>{result}</h1>
+          <h1 style={{ fontSize: "5rem", color: "grey" }}>You choose <span style={{ color: "red" }}>{playerChoice}</span> and the computer chose <span style={{ color: "gold" }}>{botChoice}</span></h1>
+          <button style={{ fontSize: "5rem", cursor: "pointer "  ,color: "grey"  }} onClick={reset}>reset</button>
+          <h1 style={{ fontSize: "5rem", textDecoration: "underlined", color: "green" }}>{result}</h1>
         </div>}
     </>
   )
--- src/components/playerchoice/PlayerChoice.jsx
+++ src/components/playerchoice/PlayerChoice.jsx
@@ -1,12 +1,20 @@
 import "./PlayerChoice.css"
-export default function PlayerChoice ({clickedUserChoice}) {
+import spock from "../../../public/images/icon-spock.svg"
+import rock from "../../../public/images/icon-rock.svg"
+import lizard from "../../../public/images/icon-lizard.svg"
+import scissor from "../../../public/images/icon-scissors.svg"
+import paper from "../../../public/images/icon-paper.svg"
+
+
+
+export default function PlayerChoice ({clickedUserChoice , userChoice , botChoice , mainMenu}) {
     return (
-        <div className="player-buttons">
-            <button onClick={() => clickedUserChoice("spock")} className="player-button   bg-amber-950">spock</button>
-            <button onClick={() => clickedUserChoice("rock")} className="player-button    bg-amber-950">rock</button>
-            <button onClick={() => clickedUserChoice("lizard")} className="player-button  bg-amber-950">lizard</button>
-            <button onClick={() => clickedUserChoice("scissor")} className="player-button bg-amber-950">scissor</button>
-            <button onClick={() => clickedUserChoice("paper")} className="player-button   bg-amber-950">paper</button>
+        <div className="player-buttons choices" style={{padding: "2rem"}}>
+            <img onClick={() => clickedUserChoice("spock")} className="player-button " src={spock} alt="spock" />
+            <img onClick={() => clickedUserChoice("rock")} className="player-button  " src={rock} alt="rock" />
+            <img onClick={() => clickedUserChoice("lizard")} className="player-button" src={lizard} alt="lizard" />
+            <img onClick={() => clickedUserChoice("scissor")} className="player-button"src={scissor} alt="scissor" />
+            <img onClick={() => clickedUserChoice("paper")} className="player-button " src={paper} alt="paper" />
         </div>
     )
 } 
